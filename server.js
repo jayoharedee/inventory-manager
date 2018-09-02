4831e1d2c2cb759706f5862b0e754b94336b4d18
Jordan Morenstein <jordan@caxap.design>
2018-09-02 00:49:26 -0400

    passport setup

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
+
+const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB config
+const db = require('./config/keys').mongoURI;
+const port = process.env.NODE_PORT || 5000;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err));
+
+
+app.use(passport.initialize());
+
+
+require('./config/passport')(passport);
+
+app.use('/api/users', users);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
